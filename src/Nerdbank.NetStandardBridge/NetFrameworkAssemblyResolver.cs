49a33fdda7137d99ea59c91b78640474a0fe461b
Andrew Arnott <andrew.arnott@microsoft.com>
2024-04-22 16:36:49 -0600

    Fix compiler warnings

--- src/Nerdbank.NetStandardBridge/NetFrameworkAssemblyResolver.cs
+++ src/Nerdbank.NetStandardBridge/NetFrameworkAssemblyResolver.cs
@@ -53,9 +53,9 @@ public class NetFrameworkAssemblyResolver
         IEnumerable<XElement> dependentAssemblies = assemblyBinding?.Elements(XName.Get("dependentAssembly", Xmlns)) ?? Enumerable.Empty<XElement>();
         foreach (XElement dependentAssembly in dependentAssemblies)
         {
-            XElement assemblyIdentity = dependentAssembly.Element(XName.Get("assemblyIdentity", Xmlns));
-            XElement codeBase = dependentAssembly.Element(XName.Get("codeBase", Xmlns));
-            XElement bindingRedirect = dependentAssembly.Element(XName.Get("bindingRedirect", Xmlns));
+            XElement? assemblyIdentity = dependentAssembly.Element(XName.Get("assemblyIdentity", Xmlns));
+            XElement? codeBase = dependentAssembly.Element(XName.Get("codeBase", Xmlns));
+            XElement? bindingRedirect = dependentAssembly.Element(XName.Get("bindingRedirect", Xmlns));
             if (assemblyIdentity is null)
             {
                 continue;
@@ -350,9 +350,14 @@ public class NetFrameworkAssemblyResolver
     /// that will assist in finding and loading assemblies based on the rules in the configuration file this instance was initialized with.
     /// </summary>
     /// <param name="loadContext">The load context to add a handler to.</param>
-    /// <param name="blockMoreResolvers"><c>true</c> to block other <see cref="AssemblyLoadContext.Resolving"/> event handlers from being effectively added.</param>
+    /// <param name="blockMoreResolvers"><see langword="true"/> to block other <see cref="AssemblyLoadContext.Resolving"/> event handlers from being effectively added.</param>
     public void HookupResolver(AssemblyLoadContext loadContext, bool blockMoreResolvers)
     {
+        if (loadContext is null)
+        {
+            throw new ArgumentNullException(nameof(loadContext));
+        }
+
         loadContext.Resolving += (s, assemblyName) => this.Load(assemblyName);
 
         if (blockMoreResolvers)
