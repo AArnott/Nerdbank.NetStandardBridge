a7ead9698a51ee2ef112383cbf56eac5d077079e
Andrew Arnott <andrew.arnott@microsoft.com>
2024-04-25 13:02:26 -0600

    Bring back copyright notice

--- src/Nerdbank.NetStandardBridge/NetFrameworkAssemblyResolver.cs
+++ src/Nerdbank.NetStandardBridge/NetFrameworkAssemblyResolver.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+// Copyright (c) Microsoft Corporation. All rights reserved.
 using System.Collections.Concurrent;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
